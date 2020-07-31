540399a12acb42cbab723365b3e37ba4a0c14483
RenattoNL <renattonl@gmail.com>
2020-07-31 13:09:25 -0500

    proyecto terminado

---
+++ src/components/Footer.svelte
@@ -0,0 +1,14 @@
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+
+<div class="Footer">
+  <div class="Footer-container">
+    <div class="Footer-copy">c 2020 Pugstagram from platzi</div>
+  </div>
+</div>
--- src/components/TimeLine.svelte
+++ src/components/TimeLine.svelte
@@ -1,1 +1,20 @@
-<div class="TimeLine">TimeLine</div>
+<script>
+  import Card from "./Card.svelte";
+  export let posts = [];
+</script>
+
+<style>
+  .TimeLine {
+    padding: 4em 0 0 0;
+  }
+</style>
+
+<div class="TimeLine">
+  <div class="timeLina-container">
+    {#each posts as post}
+      <Card {...post} />
+    {:else}
+      <p>Loading...</p>
+    {/each}
+  </div>
+</div>
